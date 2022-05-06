87ced72769143503b45e69ee4b78be848d431ec3
Fernando <fernandoleonett@gmail.com>
2022-05-06 00:13:20 -0300

    branch wit additional features

--- app/src/main/java/com/codelab/luckydice/MainActivity.kt
+++ app/src/main/java/com/codelab/luckydice/MainActivity.kt
@@ -1,57 +1,113 @@
 package com.codelab.luckydice
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.Button
-import android.widget.ImageView
-import android.widget.TextView
+import android.widget.*
+import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
 
-    /**
-     * This method is called when the Activity is created.
-     */
+
     override fun onCreate(savedInstanceState: Bundle?) {
+
+
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        // Find the Button in the layout
+        val textResult: TextView = findViewById(R.id.textResult)
+        val diceImage1: ImageView = findViewById(R.id.imageView)
+        val diceImage2: ImageView = findViewById(R.id.imageView2)
         val rollButton: Button = findViewById(R.id.button)
+        textResult.text = "Welcome !!"
 
-        // Set a click listener on the button to roll the dice when the user taps the button
-        rollButton.setOnClickListener { rollDice() }
+        var diceRoll1 = 0
+        var diceRoll2 = 0
+        var count = 0;
+//        var msg = when (count) {
+//            0 -> "Welcome"
+//            else -> "this is yor $count attempt Try Again 😣"
+//        }
 
-        // Do a dice roll when the app starts
-        rollDice()
-    }
 
-    /**
-     * Roll the dice and update the screen with the result.
-     */
-    private fun rollDice() {
-        // Create new Dice object with 6 sides and roll it
-        val dice = Dice(6)
-        val diceRoll = dice.roll()
-
-        // Find the ImageView in the layout
-        val diceImage: ImageView = findViewById(R.id.imageView)
-        val diceText: TextView = findViewById(R.id.textView)
-
-        // Determine which drawable resource ID to use based on the dice roll
-        val drawableResource = when (diceRoll) {
-            1 -> R.drawable.dice_1
-            2 -> R.drawable.dice_2
-            3 -> R.drawable.dice_3
-            4 -> R.drawable.dice_4
-            5 -> R.drawable.dice_5
-            else -> R.drawable.dice_6
+        fun getImageResource(diceRoll: Int): Int {
+            val drawableResource1 = when (diceRoll) {
+                1 -> R.drawable.dice_1
+                2 -> R.drawable.dice_2
+                3 -> R.drawable.dice_3
+                4 -> R.drawable.dice_4
+                5 -> R.drawable.dice_5
+                else -> R.drawable.dice_6
+            }
+            return drawableResource1
         }
 
-        // Update the ImageView with the correct drawable resource ID
-        diceImage.setImageResource(drawableResource)
-        diceText.text =diceRoll.toString()
+        fun rollDice() {
 
-        // Update the content description
-        diceImage.contentDescription = diceRoll.toString()
+
+            count++
+            val dice = Dice(6)
+            diceRoll1 = 0;
+            diceRoll2 = 0;
+
+            while (diceRoll1 == diceRoll2) {
+
+                diceRoll1 = dice.roll() // roll the dice
+                diceRoll2 = dice.roll()
+            }
+            val drawableResource1 = getImageResource(diceRoll1)
+            val drawableResource2 = getImageResource(diceRoll2)
+            Toast.makeText(this,"entre : $diceRoll1 y: $diceRoll2 ",Toast.LENGTH_LONG).show()
+
+            diceImage1.setImageResource(drawableResource1)
+            diceImage1.setImageResource(drawableResource2)
+
+
+            diceImage1.contentDescription = diceRoll1.toString()
+            diceImage2.contentDescription = diceRoll2.toString()
+
+
+        }
+        if (count == 0) {
+            rollDice()
+        }
+        fun play() {
+
+
+            val userInput =
+                findViewById<EditText>(R.id.editTextNumber)
+
+            val userNumber = userInput.text.toString().toInt()
+            var msg = "";
+
+            if (!userInput.text.isNullOrBlank()) {
+                if (userNumber in 1..6 && userNumber in diceRoll1..diceRoll2 || userNumber in diceRoll2..diceRoll1) {
+
+                    msg = "Haz acertado 👏👏"
+                    rollDice()
+                } else if (userNumber < 1 || userNumber > 6) {
+                    msg = "Te notifico con un dado solo tiene numeros del 1 al 6🤨"
+                } else {
+                    msg = "Buen intento 😅";
+                }
+                textResult.text = msg
+
+
+            } else {
+                Toast.makeText(this,"Escribe algo sopenco 🤨",Toast.LENGTH_LONG).show()
+            }
+
+
+            // Do a dice roll when the app starts
+
+        }
+
+        rollButton.setOnClickListener {
+
+            play()
+        }
+
+
+//
+
+
     }
 }
